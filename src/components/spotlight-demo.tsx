7e36d1801955af329063c16a9cefe8a8707409d8
Urvil Patel <uvpatel7271@gmail.com>
2026-08-10 17:37:54 +0530

    Description and proper changes

--- src/components/spotlight-demo.tsx
+++ src/components/spotlight-demo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Spotlight } from "@/components/ui/spotlight";
+import NoiseBackgroundDemo from "./noise-background-demo";
 
 export default function SpotlightPreview() {
   return (
@@ -17,15 +18,13 @@ export default function SpotlightPreview() {
         fill="white"
       />
       <div className="relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
-        <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
-          Spotlight <br /> is the new trend.
+        <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-3xl font-bold text-transparent md:text-7xl">
+       Agency CRM <br /> 
         </h1>
-        <p className="mx-auto mt-4 max-w-lg text-center text-base font-normal text-neutral-300">
-          Spotlight effect is a great way to draw attention to a specific part
-          of the page. Here, we are drawing the attention towards the text
-          section of the page. I don&apos;t know why but I&apos;m running out of
-          copy.
+        <p className="mx-auto mt-6 max-w-lg text-center text-base font-normal text-neutral-300">
+          Turn Every Lead Into a Lasting Customer. Manage leads, automate follow-ups, track your sales pipeline, and build stronger customer relationships—all from one powerful CRM workspace.
         </p>
+        <NoiseBackgroundDemo />
       </div>
     </div>
   );
